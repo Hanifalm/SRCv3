df6439229bdeeb5261a102b8dccc0b1b42ad0fae
Hanifalm <156154883+Hanifalm@users.noreply.github.com>
2025-03-01 15:18:32 +0700

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -1,17 +1,4 @@
 
-# File Name: get_func.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-02-01
-# Version: 2.0.5
-# License: MIT License
-# Improved logic handles
----------------------------------------------------
 
 import asyncio
 import time
